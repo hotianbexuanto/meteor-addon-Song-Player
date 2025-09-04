63be78d2dea943abe921049aec4c086e2ae5c9ad
hotianbexuanto <1772720865@qq.com>
2025-09-04 14:41:33 +0800

    feat(songplayer): add song selection setting and improve module activation logic
    - Add a new setting to select songs from the songs directory
    - Improve module activation to automatically load selected song
    - Enhance deactivation logic to properly stop and cleanup songs
    - Include utility methods to list available songs in the directory

--- src/main/java/hotian/addon/songplayer/modules/SongPlayerModule.java
+++ src/main/java/hotian/addon/songplayer/modules/SongPlayerModule.java
@@ -6,21 +6,76 @@ import hotian.addon.songplayer.songplayer.playing.SongHandler;
 import hotian.addon.songplayer.songplayer.CommandProcessor;
 import hotian.addon.songplayer.songplayer.SongPlayer;
 import hotian.addon.songplayer.songplayer.Config;
+import meteordevelopment.meteorclient.settings.StringSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.ProvidedStringSetting;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SongPlayerModule extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<String> songName = sgGeneral.add(new ProvidedStringSetting.Builder()
+            .name("song-name")
+            .description("The name of the song to play")
+            .defaultValue("")
+            .supplier(this::getAvailableSongsArray)
+            .build()
+    );
+
     public SongPlayerModule() {
-        super(SongPlayerAddon.CATEGORY, "Song Player", "A module to play songs with noteblocks.");
+        super(SongPlayerAddon.CATEGORY, "song-player", "A module to play songs with noteblocks.");
     }
 
     @Override
     public void onActivate() {
-        // Initialize SongPlayer when the module is activated
-        // This ensures the SongPlayer system is ready to handle play requests
+        // Play the song when the module is activated
+        if (!songName.get().isEmpty()) {
+            SongHandler.getInstance().loadSong(songName.get());
+        }
     }
 
     @Override
     public void onDeactivate() {
-        // Clean up when the module is deactivated
-        SongHandler.getInstance().reset();
+        // Stop the song when the module is deactivated using the same logic as the "stop" command
+        if (!SongHandler.getInstance().isIdle()) {
+            // This replicates the logic from CommandProcessor.stopCommand
+            if (SongHandler.getInstance().cleaningUp) {
+                SongHandler.getInstance().restoreStateAndReset();
+            } else if (Config.getConfig().autoCleanup && SongHandler.getInstance().originalBlocks.size() != 0) {
+                SongHandler.getInstance().partialResetAndCleanup();
+            } else {
+                SongHandler.getInstance().restoreStateAndReset();
+            }
+        }
+    }
+    
+    // Get list of available songs from the songs directory as an array
+    private String[] getAvailableSongsArray() {
+        List<String> songs = getAvailableSongs();
+        return songs.toArray(new String[0]);
+    }
+    
+    // Get list of available songs from the songs directory
+    public static List<String> getAvailableSongs() {
+        try {
+            if (!Files.exists(SongPlayer.SONG_DIR)) {
+                return new ArrayList<>();
+            }
+            
+            return Files.list(SongPlayer.SONG_DIR)
+                    .filter(Files::isRegularFile)
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
     }
 }
